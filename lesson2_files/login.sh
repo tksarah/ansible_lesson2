b20ae12275f7ac9e0fbbe93905cec594c69c35e4
tksarah <tsarah8139@gmail.com>
2016-05-11 16:43:15 +0900

    Update 20160511

--- lesson2_files/login.sh
+++ lesson2_files/login.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-HOST="${USER}_${1}"
 
 case $1 in
         host)
                 echo
                 echo "Login Ansible Host... "
                 echo
+		HOST=${USER}_l2_${1}
                 docker exec -it $HOST bash
                 echo ;;
         target)
                 echo
                 echo "Login Ansible Target... "
                 echo
+		HOST="${USER}_l2_${1}"
                 docker exec -it $HOST bash
                 echo ;;
         *)
--- lesson2_files/tools/setup.sh
+++ lesson2_files/tools/setup.sh
@@ -2,7 +2,7 @@
 
 INVENTORY=$HOME/lesson2_files/tools/hosts
 PLAYBOOK=$HOME/lesson2_files/tools/playbook.yml
-HOSTADDR="192.168.0.123"
+HOSTADDR="192.168.166.210"
 WP="${USER}_wordpress"
 
 case $1 in
