50417aed92b4cd38e5b3b71e100d996b8c0dd965
tksarah <tsarah8139@gmail.com>
2016-05-16 13:43:42 +0900

    Update 20160516

---
+++ dockerfile/create_proxy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+HOST="192.168.166.210"
+FILE="myproxy"
+
+cat << EOS > $FILE
+server {
+    listen 80;
+    server_name _;
+
+    root /var/www/html;
+    index index.html;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+EOS
+
+for USER in `cat userlist`
+	do
+
+	CPORT=`/bin/docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}} {{$p}} -> {{(index $conf 0).HostPort}} {{end}}' ${USER}_l1_target | gawk '{ print $3 }'`
+	#CIP=`/bin/docker inspect --format '{{.NetworkSettings.IPAddress}}' "${USER}_l1_target"`
+
+	echo "    location /${USER}_wordpress/ {" >> $FILE
+	echo "        proxy_pass http://$HOST:$CPORT/wordpress/;" >> $FILE
+	echo "    }" >> $FILE
+	done
+
+echo "}" >> $FILE
