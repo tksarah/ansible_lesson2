ab97940fd374a704e1c6ff8b2ad1863ff657dbbe
tksarah <tsarah8139@gmail.com>
2016-05-19 16:51:08 +0000

    Update 201605200153

--- lesson2_files/tools/initial.sh
+++ lesson2_files/tools/initial.sh
@@ -3,8 +3,10 @@
 INVENTORY=$HOME/lesson2_files/tools/hosts
 PLAYBOOK=$HOME/lesson2_files/tools/playbook.yml
 
-echo "Initializing ..."
-ansible-playbook -i $INVENTORY -t bootup -e user=$USER $PLAYBOOK $2
+PORT=$1
+
+echo "Initializing ... $USER $PORT"
+ansible-playbook -i $INVENTORY -t bootup -e "user=$USER cport=$PORT" $PLAYBOOK $2
 CPORT=`/bin/docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}} {{$p}} -> {{(index $conf 0).HostPort}} {{end}}' ${USER}_l2_target | gawk '{ print $3 }'`
 CIP=`/bin/docker inspect --format '{{.NetworkSettings.IPAddress}}' "${USER}_l2_target"`
 echo "Target Container IP Address : $CIP"
